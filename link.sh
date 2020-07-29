8deaf0347fa0fa8fb18df8fb47b113815d5b55e4
a180024 <a180024@e.ntu.edu.sg>
2020-07-30 04:58:29 +0800

    fix symlink

--- link.sh
+++ link.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 
-find . -type f -name '.*' -exec ln -sv ~ {} \;
-       	
-#alternatively, find . -name ".*" -print0 | while read -d $\0' file do ... using "$file" done
-
-
+find ~+ -type f -name ".*" -exec ln -sv {} ~ \;
