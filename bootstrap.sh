f4e5c8f8e928d4b0ab754db4bdab61305f1eb395
a180024 <a180024@e.ntu.edu.sg>
2020-07-30 17:34:33 +0800

    update

---
+++ bootstrap.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+find ~+ -type f -name ".*" -exec ln -sv {} ~ \;
+
+#cd ~/
+#git pull origin master
+#function sync() {
+    #rsync --exclude "README.md" --exclude ".git/" . ~;
+    #source ~/.zshrc;
+#}
+
+#if [ "$1" == "--force" -o "$1" == "-f" ]; then
+    #sync();
+#else
+    #read -p "This may overwrite existing files in your home directory. Are you sure? (y/n)" -n 1;
+    #echo "";
+    #if [[ $REPLY =~ ^[Yy]$ ]]; then
+        #sync;
+    #fi;
+#fi;
+#unset sync;
--- link.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-find ~+ -type f -name ".*" -exec ln -sv {} ~ \;
